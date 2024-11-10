11db22dad78f55d3af11443d92dc7b19e3beb1b1
maplepl <guntherkaede08@gmail.com>
2024-11-10 15:31:48 +0800

    added js

---
+++ api-calls/script.js
@@ -0,0 +1,6 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;    console.log(finalURL);
+});
